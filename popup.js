a77814bd0ed08e250feb692ca89a2d64b59d0dff
Nitin Pasumarthy <nithanaroy@gmail.com>
2014-10-05 16:08:41 -0700

    Enabled Sync storage

---
+++ popup.js
@@ -0,0 +1,32 @@
+$(function() {
+
+	chrome.storage.sync.get('total', function(items) {
+		var new_total = 0;
+		if (items.total) {
+			$("#total").text(items.total);
+		}
+	});
+
+	$("#addAmount").click(function() {
+		chrome.storage.sync.get('total', function(items) {
+			var new_total = 0;
+			if (items.total) {
+				new_total = parseInt(items.total);
+			}
+
+			var amt = parseInt($("#amount").val());
+			if (amt) {
+				new_total += amt;
+			};
+
+			// Save the new settings
+			chrome.storage.sync.set({
+				'total': new_total
+			});
+
+			// Update UI
+			$("#total").text(new_total);
+			$("#amount").val("");
+		});
+	});
+});
